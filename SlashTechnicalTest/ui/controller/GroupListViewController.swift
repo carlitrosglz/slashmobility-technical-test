f22a4f93ef3cad47c1c1ce4e8cb18bab38420a0e
Carlos G <carlos.gonzalez@suiff.com>
2021-12-30 19:35:26 +0100

    Se continua el desarrollo de los layouts.

--- SlashTechnicalTest/ui/controller/GroupListViewController.swift
+++ SlashTechnicalTest/ui/controller/GroupListViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class GroupListViewController: UIViewController {
-
+    private let TAG = String(describing: self)
+    
+    @IBOutlet weak var table_view: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    private func configureView() {
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        table_view.delegate   = self
+        table_view.dataSource = self
+        table_view.rowHeight  = 200
     }
     
 
@@ -26,4 +39,35 @@ class GroupListViewController: UIViewController {
     }
     */
 
+}
+
+extension GroupListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Navigate to GroupDetailViewController
+        performSegue(withIdentifier: "segueToGroupDetailViewController", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+}
+
+extension GroupListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = table_view.dequeueReusableCell(withIdentifier: GroupListCell.reuseIdentifier, for: indexPath) as? GroupListCell else {
+            fatalError("The dequeued cell is not an instance of GroupListCell.")
+        }
+
+        // cell.setData()
+        
+        return cell
+    }
 }
