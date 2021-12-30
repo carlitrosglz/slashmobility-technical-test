de84b41f2dfd30a4eaacb71885c69ee3e14e603b
Carlos G <carlos.gonzalez@suiff.com>
2021-12-30 18:15:55 +0100

    Initial Commit

---
+++ SlashTechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlashTechnicalTest
+//
+//  Created by Innova Suiff on 30/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
